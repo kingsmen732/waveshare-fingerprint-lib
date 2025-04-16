b49a03e7e192089aeb748085e89ded4ea18a44f7
kingsmen732 <mitumithilesh2@gmail.com>
2025-04-16 21:48:17 +0000

    commit  --stable

---
+++ target/debug/deps/waveshare_fingerprint_lib-3350b81a89a1c091.d
@@ -0,0 +1,7 @@
+/home/darkside/key/waveshare-fingerprint-lib/target/debug/deps/libwaveshare_fingerprint_lib-3350b81a89a1c091.rmeta: src/lib.rs
+
+/home/darkside/key/waveshare-fingerprint-lib/target/debug/deps/libwaveshare_fingerprint_lib-3350b81a89a1c091.rlib: src/lib.rs
+
+/home/darkside/key/waveshare-fingerprint-lib/target/debug/deps/waveshare_fingerprint_lib-3350b81a89a1c091.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/libwaveshare_fingerprint_lib.d
@@ -0,0 +1,1 @@
+/home/darkside/key/waveshare-fingerprint-lib/target/debug/libwaveshare_fingerprint_lib.rlib: /home/darkside/key/waveshare-fingerprint-lib/src/lib.rs
